cf07dae7aa3f8354776cc9ce052da95aaa3bf60c
Liam Esseen <Liam.esseen@elev.ga.ntig.se>
2023-08-31 10:26:54 +0200

    Första committen

---
+++ Övning/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+
+Console.WriteLine("Vad heter du?");
+
+string name = Console.ReadLine();
+Console.WriteLine($"Hej {name} vad bänkar du?");
+
+string tvåhundrakilo = Console.ReadLine();
+Console.WriteLine($"Ojsan {tvåhundrakilo} är tungt, men du är ful.");
+
+string smusk = Console.ReadLine();
+Console.WriteLine($"Även fast du säger {smusk} så är du fin som du är, säg det tillbaka");
+
+string nicevikt = Console.ReadLine();
+Console.WriteLine($"Tack så mycket din fina fina lurre, hur mår du?");
+
+string yo = Console.ReadLine();
+Console.WriteLine($"att må {yo} är inte så bra, men kan vara bra ibland!, är jag fin?");
+
+string smygfis = Console.ReadLine();
+Console.WriteLine($"Nämen tackar för att du sa {smygfis} men du är full av skit. svara inte");
+
+string fining = Console.ReadLine();
+Console.WriteLine($"Jag SA SVARA INTE, men du sa {fining} nice ...");
+
+string smygg = Console.ReadLine();
+Console.WriteLine($"Tyst.");
+
+string JKA = Console.ReadLine();
+Console.WriteLine($"tackar för att du sa {JKA} jag kommer inte glömma det, smusk");
+
+string TTT = Console.ReadLine();
+Console.WriteLine($"Tack för {TTT} din fuling");
+
+string smuttts = Console.ReadLine();
+Console.WriteLine($"Asså {smuttts} är fint ändå");
+
+string LMF = Console.ReadLine();
+Console.WriteLine($"Tack så mycket din fina fina lurre");
+
+Console.WriteLine("Liam");
+
+Console.ReadLine();
